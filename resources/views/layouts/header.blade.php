100b4f51e5460ddffcfa677452c6aa1f9ca7876c
blueswayne133 <blueswayne133@gmail.com>
2025-06-25 11:36:53 -0700

    new changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,18 +19,7 @@
     <title>Neoglobal Trust Bank- Money Transfer & Banking</title>
     <link rel="icon" type="image/png" href="asset/img/favicon.png">
 
-    <!-- Smartsupp Live Chat script -->
-    <script type="text/javascript">
-        var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'a32031170cef4293e7bdf2197c91cbf908e92bdd';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-    </script>
-    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </head>
 
 <body>
